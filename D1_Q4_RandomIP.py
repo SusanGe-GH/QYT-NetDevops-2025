ac2131e485e20aefc6de1671a009ec5a04b2b19a
SusanGe-GH <geshanshan_susan@163.com>
2025-02-22 14:43:16 +0800

    Add files via upload

---
+++ D1_Q4_RandomIP.py
@@ -0,0 +1,8 @@
+# This is the D1 Homework for GSS
+# 要求：导入random模块，随机产生网络IPv4地址。
+
+import random
+
+# random_ip = str(random.randint(0,255)) + "." + str(random.randint(0,255)) + "." + str(random.randint(0,255)) + "." + str(random.randint(0,255))
+print(str(random.randint(0,255)) + "." + str(random.randint(0,255)) + "." + str(random.randint(0,255)) + "." + str(random.randint(0,255)))
+
