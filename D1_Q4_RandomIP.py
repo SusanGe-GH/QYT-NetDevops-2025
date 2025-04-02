9a26c56a611fdbb400f6358cdbe0b11afae31bf9
SusanGe-GH <geshanshan_susan@163.com>
2025-04-02 11:28:45 +0800

    Delete D1_Q4_RandomIP.py

--- D1_Q4_RandomIP.py
+++
@@ -1,8 +0,0 @@
-# This is the D1 Homework for GSS
-# 要求：导入random模块，随机产生网络IPv4地址。
-
-import random
-
-# random_ip = str(random.randint(0,255)) + "." + str(random.randint(0,255)) + "." + str(random.randint(0,255)) + "." + str(random.randint(0,255))
-print(str(random.randint(0,255)) + "." + str(random.randint(0,255)) + "." + str(random.randint(0,255)) + "." + str(random.randint(0,255)))
-
